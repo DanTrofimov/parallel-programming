3b6bf24ef1cd671a4c36b70df0c1e760dc465671
Daniil Trofimov <trofimovdanil946@gmail.com>
2021-11-16 01:53:33 +0300

    fixes

--- ConsoleApplication1/ConsoleApplication1/mpi.cpp
+++ ConsoleApplication1/ConsoleApplication1/mpi.cpp
@@ -446,10 +446,10 @@ void twelfth_task() {
 }
 
 int main(int argc, char** argv) {
-	/*MPI_Init(&argc, &argv);
+	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);*/
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	first_task();
 	MPI_Finalize();
 	return 0;
-}
+}	
