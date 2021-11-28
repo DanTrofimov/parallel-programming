3b679583b834163160e92fa53e30f4f440afa349
Daniil Trofimov <trofimovdanil946@gmail.com>
2021-11-28 19:21:20 +0300

    mpi unp

--- ConsoleApplication1/ConsoleApplication1/mpi.cpp
+++ ConsoleApplication1/ConsoleApplication1/mpi.cpp
@@ -1,20 +1,17 @@
 #include <iostream>
 #include <mpi.h>
 
+// current thread, threads amount
 int rank, size;
-const int array_size = 10000;
+const int array_size = 3;
 
-void fillArray(int* array) {
-	for (int i = 0; i < array_size; i++) {
-		array[i] = rand();
-	}
+// Hello world из всех процессов 
+void task1() {
+	printf("Hello world, size = %d, rank = %d \n", size, rank);
 }
 
-void first_task() {
-	printf("Hello, world! size = %d, rank = %d \n", size, rank);
-}
-
-void second_task() {
+// Maксимум массива  
+void task2() {
 	int array[array_size];
 	if (rank == 0) {
 		for (int i = 0; i < array_size; i++) {
@@ -40,7 +37,8 @@ void second_task() {
 	}
 }
 
-void third_task() {
+// Вычисление числа Пи методом Монте-Карло
+void task3() {
 	int radius = 100;
 	int square_side = radius * 2;
 	int point_in_circle_one_rank = 0;
@@ -59,7 +57,8 @@ void third_task() {
 	}
 }
 
-void fourth_task() {
+// Среднее арифметическое среди положительных чисел массива 
+void task4() {
 	int offset = array_size / size;
 	int buffer_size = offset;
 	int* buffer = new int[buffer_size];
@@ -111,156 +110,8 @@ void fourth_task() {
 	}
 }
 
-// exception
-void fifth_task() {
-	const int matrix_size = array_size * array_size;
-	int offset = matrix_size / size;
-	int* scounts = new int[size];
-	int* displs = new int[size];
-	for (int i = 0; i < size - 1; i++) {
-		scounts[i] = offset;
-		displs[i] = i * offset;
-	}
-	scounts[size - 1] = offset + matrix_size % size;
-	displs[size - 1] = (size - 1) * matrix_size % size;
-	int* matrixA = new int[array_size * array_size];
-	int* matrixB = new int[array_size * array_size];
-	int* buffer_matrixA = new int[matrix_size / size];
-	int* buffer_matrixB = new int[matrix_size / size];
-	long* time_c = new long[matrix_size / size];
-	if (rank == 0) {
-		matrixA = new int[array_size * array_size];
-		matrixB = new int[array_size * array_size];
-		for (int i = 0; i < array_size; i++) {
-			for (int k = 0; k < array_size; k++) {
-				matrixA[i * array_size + k] = rand() % 100;
-				matrixB[k * array_size + i] = rand() % 100;
-			}
-		}
-	}
-
-	for (int i = 0; i < size; i++) {
-
-	}
-	MPI_Scatterv(matrixA, scounts, displs, MPI_INT, buffer_matrixA, matrix_size / size, MPI_INT, 0, MPI_COMM_WORLD);
-	MPI_Scatterv(matrixB, scounts, displs, MPI_INT, buffer_matrixB, matrix_size / size, MPI_INT, 0, MPI_COMM_WORLD);
-	for (int i = 0; i < array_size / size; i++) {
-		for (int k = 0; k < array_size; k++) {
-			time_c[i * array_size + k] = 0;
-			for (int t = 0; t < array_size; t++) {
-				int c_index = i * array_size + k;
-				int a_index = i * array_size + t;
-				int b_index = k * array_size + t;
-				time_c[c_index] += (buffer_matrixA[a_index] * buffer_matrixB[b_index]) % 100;
-				/*printf("a[%d][%d] = %d, b[%d][%d] = %d, c[%d][%d] = %d \n", i, t, buffer_matrixA[a_index], t, k, buffer_matrixB[b_index], i, k, time_c[c_index]);*/
-			}
-		}
-	}
-	long* c = new long[matrix_size];
-	MPI_Gather(time_c, matrix_size / size, MPI_LONG, c, matrix_size / size, MPI_LONG, 0, MPI_COMM_WORLD);
-	if (rank == 0) {
-		for (int i = 0; i < array_size; i++) {
-			for (int k = 0; k < array_size; k++) {
-				printf("%d ", c[i * array_size + k]);
-			}
-			printf(" \n");
-		}
-	}
-}
-
-void sixth_task() {
-	const int matrix = array_size * array_size;
-	const int offset = matrix / size;
-	int buffer_size = offset;
-	int* buffer = new int[buffer_size];
-	int* scounts = new int[size];
-	int* displs = new int[size];
-	int* array = new int[matrix];
-	for (int i = 0; i < size - 1; i++) {
-		scounts[i] = offset;
-		displs[i] = i * offset;
-	}
-	scounts[size - 1] = offset + array_size % size;
-	displs[size - 1] = (size - 1) * array_size / size;
-	if (rank == 0) {
-		for (int i = 0; i < matrix; i++) {
-			array[i] = rand() % 100;
-		}
-	}
-	for (int i = 0; i < size; i++) {
-		if (rank == i) {
-			buffer = new int[scounts[i]];
-			break;
-		}
-	}
-	MPI_Scatterv(array, scounts, displs, MPI_INT, buffer, buffer_size, MPI_INT, 0, MPI_COMM_WORLD);
-	int minmax = buffer[0];
-	int maxmin = buffer[0];
-	for (int i = 0; i < array_size / size; i++) {
-		int min = buffer[i * array_size];
-		int max = buffer[i * array_size];
-		for (int k = 0; k < array_size; k++) {
-			if (min > buffer[i * array_size + k]) min = buffer[i * array_size + k];
-			if (max < buffer[i * array_size + k]) max = buffer[i * array_size + k];
-		}
-		if (maxmin < min) maxmin = min;
-		if (minmax > max) minmax = max;
-	}
-	int time_buffer[2] = { maxmin, minmax };
-	int* min_max = new int[2 * size];
-	MPI_Gather(time_buffer, 2, MPI_INT, min_max, 2, MPI_INT, 0, MPI_COMM_WORLD);
-	maxmin = min_max[0];
-	minmax = min_max[1];
-	if (rank == 0) {
-		for (int i = 0; i < 2 * size; i++) {
-			if (i % 2 == 0 && maxmin < buffer[i]) maxmin = buffer[i];
-			if (i % 2 != 0 && minmax > buffer[i]) minmax = buffer[i];
-		}
-		printf("maxmin = %d, minmax = %d \n", maxmin, minmax);
-	}
-}
-
-void seventh_task() {
-	const int matrix = array_size * array_size;
-	const int offset = matrix / size;
-	int buffer_size = offset;
-	int* buffer_matrix = new int[buffer_size];
-	int* buffer_vector = new int[array_size / size];
-	int* vector = new int[array_size];
-	int* array = new int[matrix];
-	if (rank == 0) {
-		for (int i = 0; i < array_size; i++) {
-			vector[i] = rand() % 100;
-			for (int k = 0; k < array_size; k++) {
-				array[k * array_size + i] = rand() % 100;
-			}
-		}
-	}
-	MPI_Scatter(array, offset, MPI_INT, buffer_matrix, offset, MPI_INT, 0, MPI_COMM_WORLD);
-	MPI_Scatter(vector, array_size / size, MPI_INT, buffer_vector, array_size / size, MPI_INT, 0, MPI_COMM_WORLD);
-	for (int i = 0; i < array_size; i++) {
-		vector[i] = 0;
-		for (int k = 0; k < array_size / size; k++) {
-			vector[i] += (buffer_matrix[k * array_size + i] * buffer_vector[i]) % 100;
-			vector[i] %= 100;
-		}
-	}
-	int* result_vector = new int[array_size];
-	int* buffer_vector_result = new int[array_size * size];
-	MPI_Gather(vector, array_size, MPI_INT, buffer_vector_result, array_size, MPI_INT, 0, MPI_COMM_WORLD);
-	if (rank == 0) {
-		for (int i = 0; i < array_size; i++) {
-			for (int k = 0; k < size; k++) {
-				result_vector[i] += buffer_vector_result[k * array_size + i];
-				result_vector[i] %= 100;
-			}
-			printf("%d \n", result_vector[i]);
-		}
-	}
-}
-
-
-void eigth_task() {
+// Scatter и Gather через Send и Recv
+void task8() {
 	int buffer_size = array_size / size;
 	int* buffer = new int[array_size / size];
 	MPI_Status status;
@@ -305,7 +156,8 @@ void eigth_task() {
 	}
 }
 
-void ninth_task() {
+// Инвертировать массив
+void task9() {
 	int buffer_size = array_size / size;
 	int* buffer = new int[buffer_size];
 	MPI_Status status;
@@ -350,57 +202,8 @@ void ninth_task() {
 	}
 }
 
-// exception
-void tenth_task() {
-	int* array = new int[array_size];
-	int* bsend_buffer = new int[10 * array_size];
-	int* buffer = new int[array_size];
-	MPI_Status status;
-	for (int i = 0; i < array_size; i++) {
-		array[i] = rand();
-	}
-	double start_time;
-	double end_time;
-	if (rank == 0) {
-		start_time = MPI_Wtime();
-		MPI_Send(array, array_size, MPI_INT, 1, 0, MPI_COMM_WORLD);
-		end_time = MPI_Wtime();
-		printf("Send time = %.6f \n", end_time - start_time);
-	}
-	else {
-		MPI_Recv(buffer, array_size, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
-	}
-	if (rank == 0) {
-		start_time = MPI_Wtime();
-		MPI_Ssend(array, array_size, MPI_INT, 1, 1, MPI_COMM_WORLD);
-		end_time = MPI_Wtime();
-		printf("Ssend time = %.6f \n", end_time - start_time);
-	}
-	else {
-		MPI_Recv(buffer, array_size, MPI_INT, 0, 1, MPI_COMM_WORLD, &status);
-	}
-	if (rank == 0) {
-		MPI_Buffer_attach(bsend_buffer, 10 * array_size);
-		start_time = MPI_Wtime();
-		MPI_Bsend(array, array_size, MPI_INT, 1, 2, MPI_COMM_WORLD);
-		end_time = MPI_Wtime();
-		printf("Bsend time = %.6f \n", end_time - start_time);
-	}
-	else {
-		MPI_Recv(buffer, array_size, MPI_INT, 0, 2, MPI_COMM_WORLD, &status);
-	}
-	if (rank == 0) {
-		start_time = MPI_Wtime();
-		MPI_Rsend(array, array_size, MPI_INT, 1, 3, MPI_COMM_WORLD);
-		end_time = MPI_Wtime();
-		printf("Rsend time = %.6f \n", end_time - start_time);
-	}
-	else {
-		MPI_Recv(buffer, array_size, MPI_INT, 0, 3, MPI_COMM_WORLD, &status);
-	}
-}
-
-void eleventh_task() {
+// Циклическая передача данных 
+void task11() {
 	MPI_Status st;
 	int value;
 	if (rank == 0) {
@@ -420,44 +223,104 @@ void eleventh_task() {
 	}
 }
 
-// exception
-void twelfth_task() {
-	eleventh_task();
-	MPI_Finalize();
-	MPI_Init(NULL, NULL);
-	MPI_Group group;
-	MPI_Group new_group;
-	MPI_Comm comm;
-	MPI_Comm_group(MPI_COMM_WORLD, &group);
-	const int ranks[1] = { 3 };
-	MPI_Group_excl(group, 1, ranks, &new_group);
-	comm = MPI_Comm_create(MPI_COMM_WORLD, new_group, &comm);
-	MPI_Comm_size(comm, &size);
-	MPI_Comm_rank(comm, &rank);
-	MPI_Status st;
-	int value;
-	if (rank == 0) {
-		value = 1;
-		MPI_Send(&value, 1, MPI_INT, 1, 0, comm);
-		MPI_Recv(&value, 1, MPI_INT, size - 1, size - 1, comm, &st);
-		printf("value = %d, rank = %d \n", value, rank);
+
+// --- new ---
+
+// Скалярное произведение
+void task5() {
+	const int matrix_size = array_size * array_size;
+	int offset = matrix_size / size;
+	// amount of ops for each rank
+	int* scounts = new int[size];
+	// offset from the beginning of the transmit buffer
+	int* displs = new int[size];
+	for (int i = 0; i < size - 1; i++) {
+		scounts[i] = offset;
+		displs[i] = i * offset;
 	}
-	else {
-		int sender = rank - 1;
-		int reciver = rank + 1;
-		if (rank == size - 1) reciver = 0;
-		MPI_Recv(&value, 1, MPI_INT, sender, sender, MPI_COMM_WORLD, &st);
-		printf("value = %d, rank = %d \n", value, rank);
-		value += rank;
-		MPI_Send(&value, 1, MPI_INT, reciver, rank, comm);
+	scounts[size - 1] = offset + matrix_size % size;
+	displs[size - 1] = (size - 1) * matrix_size % size;
+
+	int* matrixA = new int[matrix_size];
+	int* matrixB = new int[matrix_size];
+	int* buffer_matrixA = new int[matrix_size / size];
+	int* buffer_matrixB = new int[matrix_size / size];
+
+	long* buffer_c = new long[matrix_size / size];
+	if (rank == 0) {
+		matrixA = new int[matrix_size];
+		matrixB = new int[matrix_size];
+		for (int i = 0; i < array_size; i++) {
+			for (int k = 0; k < array_size; k++) {
+				matrixA[i * array_size + k] = rand() % 10;
+				matrixB[k * array_size + i] = rand() % 10;
+			}
+		}
+	}
+
+	MPI_Scatterv(matrixA, scounts, displs, MPI_INT, buffer_matrixA, matrix_size / size, MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Scatterv(matrixB, scounts, displs, MPI_INT, buffer_matrixB, matrix_size / size, MPI_INT, 0, MPI_COMM_WORLD);
+	for (int i = 0; i < array_size / size; i++) {
+		for (int k = 0; k < array_size; k++) {
+			buffer_c[i * array_size + k] = 0;
+			for (int t = 0; t < array_size; t++) {
+				int c_index = i * array_size + k;
+				int a_index = i * array_size + t;
+				int b_index = k * array_size + t;
+				buffer_c[c_index] += buffer_matrixA[a_index] * buffer_matrixB[b_index];
+			}
+		}
+	}
+	long* matrixC = new long[matrix_size];
+	MPI_Gather(buffer_c, matrix_size / size, MPI_LONG, matrixC, matrix_size / size, MPI_LONG, 0, MPI_COMM_WORLD);
+	if (rank == 0) {
+		for (int i = 0; i < array_size; i++) {
+			for (int k = 0; k < array_size; k++) {
+				printf("%d ", matrixA[i * array_size + k]);
+			}
+			printf(" \n");
+		}
+
+		printf(" \n");
+
+
+		for (int i = 0; i < array_size; i++) {
+			for (int k = 0; k < array_size; k++) {
+				printf("%d ", matrixB[i * array_size + k]);
+			}
+			printf(" \n");
+		}
+
+		printf(" \n");
+		
+		for (int i = 0; i < array_size; i++) {
+			for (int k = 0; k < array_size; k++) {
+				printf("%d ", matrixC[i * array_size + k]);
+			}
+			printf(" \n");
+		}
 	}
 }
+
+// Maxmin матрицы
+void task6() {
+
+}
+
+// Умножение матрицы на вектор при разделении данных по столбцам
+void task7() {
+}
+
+// Время передачи для разных Send-oв
+void task10() {
+}
+
 
 int main(int argc, char** argv) {
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	fifth_task();
+	task5();
 	MPI_Finalize();
 	return 0;
 }	
